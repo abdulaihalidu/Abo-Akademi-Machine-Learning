8f4efde02d4aa38fa706d1f26861d9fa22031a7e
Halidu Abdulai <abdulaihalidu008@gmail.com>
2024-03-23 16:42:35 +0200

    Added final version of files

--- Lab 3 - Re-enforcement learning (drive a taxi)/autonomous_taxi.py
+++ Lab 3 - Re-enforcement learning (drive a taxi)/autonomous_taxi.py
@@ -2,113 +2,109 @@ import gymnasium as gym
 import numpy as np
 import random
 from IPython.display import clear_output
+import argparse
 import time
 
 class TaxiAgent:
-    def __init__(self, env_name='Taxi-v3', q_table_path=None):
+    def __init__(self, env_name='Taxi-v3', q_table_path=None, logging=False):
+        self.log = logging
         self.env = gym.make(env_name, render_mode="human")
-        if q_table_path is not None:
-            self.q_table = self.load_q_table(q_table_path)
-        else:
-            self.q_table = np.zeros([self.env.observation_space.n, self.env.action_space.n])
+        self.q_table = self.load_q_table(q_table_path) if q_table_path else np.zeros([self.env.observation_space.n, self.env.action_space.n])
         
         # Hyperparameters
         self.alpha = 0.4
         self.gamma = 0.7
         self.epsilon = 0.1
-        
-        # Metrics
-        self.all_epochs = []
-        self.all_penalties = []
-        self.all_losses = []  # Track "loss" for each episode
 
-    def load_q_table(self, path): # To avoid retraining all the time, I have saved the trained q-table
+        # Metrics
+        self.all_losses = []  # Tracks "loss" for each episode
+
+    def load_q_table(self, path):
         try:
             q_table = np.load(path)
-            print("Loaded q-table successfully!")
+            if self.log:
+                print("Loaded q-table successfully!")
             return q_table
         except FileNotFoundError:
-            print("File not found. Initializing a new Q-table.")
+            if self.log:
+                print("File not found. Initializing a new Q-table.")
             return np.zeros([self.env.observation_space.n, self.env.action_space.n])
 
-    def train(self, num_episodes=1001):
-        for i in range(1, num_episodes):
+    def train(self, num_episodes=1001, weights_file_name=None):
+        for episode in range(1, num_episodes):
             state = self.env.reset()[0]
-            epochs, penalties, reward = 0, 0, 0
-            terminated, truncated = False, False
-            total_loss = 0  # Track total "loss" for the episode
+            total_loss, penalties = 0, 0
             
-            while not terminated and not truncated:
-                if random.uniform(0, 1) < self.epsilon:
-                    action = self.env.action_space.sample()  # Explore action space
-                else:
-                    action = np.argmax(self.q_table[state])  # Exploit learned values
-                next_state, reward, terminated, truncated, info = self.env.step(action)
+            terminated = False
+            while not terminated:
+                action = random.choice(range(self.env.action_space.n)) if random.uniform(0, 1) < self.epsilon else np.argmax(self.q_table[state])
+                next_state, reward, terminated, _, _ = self.env.step(action)
                 
                 old_value = self.q_table[state, action]
                 next_max = np.max(self.q_table[next_state])
-                
-                # Update the Q-value
                 new_value = (1 - self.alpha) * old_value + self.alpha * (reward + self.gamma * next_max)
+                
                 self.q_table[state, action] = new_value
-
-                # Update total "loss"
                 total_loss += np.abs(new_value - old_value)
 
-                if reward == -10:
-                    penalties += 1
-
+                penalties += reward == -10
                 state = next_state
-                epochs += 1
+
+            self.all_losses.append(total_loss)
             
-            self.all_losses.append(total_loss)  # Append total loss for this episode
-    
-            if i % 100 == 0:
+            if self.log and episode % 100 == 0:
                 clear_output(wait=True)
-                print(f"Episode: {i}")
+                print(f"Episode: {episode}")
+            if self.log:                
+                if terminated:
+                    print("Passenger Dropped off at Destination.")
+                else:
+                    print("Agent Failed to drop off passenger at destination.")
 
-        print("Training finished.\n")
-        print("Saving learned Q-table to disk ...")
-        np.save('q_table.npy', self.q_table)
+        if weights_file_name:
+            np.save(f'{weights_file_name}.npy', self.q_table)
+            print("Training finished.\nSaving learned Q-table to disk...")
 
-    def drive(self, episodes=5, sleep_time=0.1):
+    def drive(self, episodes=5, sleep_time=1):
         total_epochs, total_penalties = 0, 0
 
         for _ in range(episodes):
             state = self.env.reset()[0]
-            epochs, penalties, reward = 0, 0, 0
-            terminated, truncated = False, False
-            clear_output(wait=True)
-            self.env.render()
-            time.sleep(sleep_time)
+            epochs, penalties = 0, 0
+            terminated = False
+            time.sleep(0.5) # Sleep momentatariy after setting up a new environment
             
-            while not terminated and not truncated:
+            while not terminated:
                 action = np.argmax(self.q_table[state])
-                state, reward, terminated, truncated, info = self.env.step(action)
-                clear_output(wait=True)
-                self.env.render()
-                time.sleep(sleep_time)  # Adjust sleep time as needed for visualization speed
+                state, reward, terminated, _, _ = self.env.step(action)
+                time.sleep(sleep_time)
 
-                if reward == -10:
-                    penalties += 1
-
+                penalties += reward == -10
                 epochs += 1
 
             total_epochs += epochs
             total_penalties += penalties
+            time.sleep(0.5) # Sleep momentarily after deliverying the passenger.
 
-            time.sleep(1.5) # Sleep momentarily after each episode.
-
-        print(f"Results after {episodes} episodes:")
-        print(f"Average timesteps per episode: {total_epochs / episodes}")
-        print(f"Average penalties per episode: {total_penalties / episodes}")
+        print(f"Results after {episodes} episodes:\nAverage timesteps per episode: {total_epochs / episodes}\nAverage penalties per episode: {total_penalties / episodes}")
 
 if __name__ == "__main__":
-    # Let's use the weights of the pretrained q_table
-    q_table_path = './q_table_weights.npy'
-    # Initialize a taxi agent
-    agent = TaxiAgent(q_table_path=q_table_path)
-    # Train the taxi
-    #agent.train(num_episodes=1001)
-    # Initiate self-drive
-    agent.drive(episodes=10, sleep_time=0.1)
+    parser = argparse.ArgumentParser(description="Train and drive a Taxi Agent.")
+    parser.add_argument('-t', '--train', action='store_true', help='Train the taxi')
+    parser.add_argument('-d', '--drive', action='store_true', help='Drive the taxi')
+    parser.add_argument('-w', '--weights', type=str, help='Pre-trained weights file name')
+    parser.add_argument('-l', '--log', action='store_true', help='Enable messages logging')
+    parser.add_argument('-e', '--drive_episodes', type=int, default=5, help='Number of episodes to drive taxi')
+    parser.add_argument('-s', '--save_weights', type=str, help='Name of file to save weights to')
+
+    args = parser.parse_args()
+
+    agent = TaxiAgent(q_table_path=args.weights, logging=args.log)
+
+    if args.train:
+        if not args.save_weights:
+            print("Please provide file name to save weights to.")
+        else:
+            agent.train(num_episodes=1001, weights_file_name=args.save_weights)
+    if args.drive:
+        agent.drive(episodes=args.drive_episodes, sleep_time=0.1)
